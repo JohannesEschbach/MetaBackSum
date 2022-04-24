3579227fd77213e93059fbbd97f4510254a976b7
eschbach <eschbach@cl.uni-heidelberg.de>
2022-04-24 19:23:34 +0200

    script with argument

--- scripts/train_backsum.sh
+++ scripts/train_backsum.sh
@@ -5,7 +5,7 @@ accelerate launch ../backsum.py \
 	  --model_name_or_path facebook/bart-base \
 	  --dataset_name cnn_dailymail \
 	  --dataset_config "3.0.0" \
-	  --output_dir ../models/backward_pretrained \
+	  --output_dir ../models/backsum_no_meta \
 	  --num_warmup_steps 400 \
 	  --max_source_length 148 \
 	  --num_train_epochs 5 \
@@ -13,8 +13,9 @@ accelerate launch ../backsum.py \
 	  --per_device_batch_size 4 \
 	  --text_column highlights \
 	  --summary_column article \
-	  --train_mode base \
+	  --train_mode $1 \
 	  --processed_data_dir ../data/backward \
 	  --dataset_perc 0.03 \
-	  --learning_rate 5e-6
+	  --learning_rate 5e-5 \
+  	  --back_learning_rate 5e-6
 
