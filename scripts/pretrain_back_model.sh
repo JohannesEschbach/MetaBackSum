d2a10b62da585d85e8281f20de2d761ea64848aa
eschbach <eschbach@cl.uni-heidelberg.de>
2022-04-25 10:36:29 +0200

    fix relative path

--- scripts/pretrain_back_model.sh
+++ scripts/pretrain_back_model.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+cd ${BASH_SOURCE[0]}
+cd ../
 accelerate test
-accelerate launch ${BASH_SOURCE[0]}/../backsum.py \
+accelerate launch backsum.py \
 	  --model_name_or_path facebook/bart-base \
 	  --dataset_name cnn_dailymail \
 	  --dataset_config "3.0.0" \
-	  --output_dir ${BASH_SOURCE[0]}/../models/backward_pretrained \
+	  --output_dir models/backward_pretrained \
 	  --num_warmup_steps 400 \
 	  --max_source_length 148 \
 	  --num_train_epochs 5 \
@@ -14,7 +16,7 @@ accelerate launch ${BASH_SOURCE[0]}/../backsum.py \
 	  --text_column highlights \
 	  --summary_column article \
 	  --train_mode base \
-	  --processed_data_dir ${BASH_SOURCE[0]}/../data/backward \
+	  --processed_data_dir data/backward \
 	  --dataset_perc 0.03 \
 	  --learning_rate 5e-5
 
