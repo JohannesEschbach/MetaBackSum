606310b2abc450eab43db5c6b9e9f8458d39921c
eschbach <eschbach@cl.uni-heidelberg.de>
2022-04-27 17:42:04 +0200

    update script

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -5,7 +5,7 @@ cd "$parent_path"
 cd ../
 accelerate test
 accelerate launch evaluate.py \
-          --model_name_or_path $1 \
+          --model_name_or_path models/$1 \
           --dataset_name cnn_dailymail \
           --dataset_config "3.0.0" \
           --max_source_length 1024 \
